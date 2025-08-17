37e42b4b2483efb11cedf2893447019b9717a353
ShaynaMBarclay <shaynambarclay@gmail.com>
2025-08-17 18:00:46 -0400

    quest icon added

--- src/components/NightElfScreen.jsx
+++ src/components/NightElfScreen.jsx
@@ -1,11 +1,27 @@
-
 import catImage from "../assets/cat.png";
+import topCatImage from "../assets/nelfsaber.png";
+import questIcon from "../assets/questicon.png";
+import leoCat from "../assets/blackcat.png";
 
 function NightElfScreen() {
   return (
     <div className="night-elf-screen">
-      <h1>Welcome to your Night Elf Birthday World! 🌙</h1>
+      <h1 className="night-elf-title">May the forest sing and the Wisps guide your birthday path 🌿🌙</h1>
       <img src={catImage} alt="Crawling Cat" className="crawling-cat" />
+
+      <img src={topCatImage} alt="Top Cat" className="top-cat" />
+
+        <div className="quest-wrapper">
+        <img src={questIcon} alt="Quest Icon" className="quest-icon" />
+        <div className="chat-bubble">
+           <img src={leoCat} alt="Leo the Cat" className="chat-cat" />
+        <span>Meow! Happy Birthday, my favorite human! Love, Leo 🐾🐾🐾</span>
+        </div>
+      </div>
+
+      {Array.from({ length: 15 }).map((_, i) => (
+        <div key={i} className={`wisp wisp${i + 1}`} />
+      ))}
     </div>
   );
 }
